1a64d990f8ccb23574bb424d052fea93c2ed98d5
lasagnaboy <githublb@salim.virji.net>
2023-11-24 09:24:50 -0500

    Update santa.go: add flag to toggle behavior
    The Santa gift assigner has two modes: stocking stuffers and grown-up gift exchange. Add a boolean flag, `-stocking`, `true` by default, to toggle modes at execution time. Use the flag to control program flow.

--- santa.go
+++ santa.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+        "flag"
         "fmt"
         "math/rand"
         "time"
 )
 
 func main() {
-        //strSantas := [...]string{"Anna", "John", "Nico", "Nicole", "Salim", "Susan" }
-        strSantas := [...]string{"Anna", "Nina", "Laila", "John", "Nico", "Nicole", "Salim", "Susan", }
+        var strSantas []string
+        s := flag.Bool("stockings", true, "a bool indicating whether to run for stocking stuffers")
+        flag.Parse()
+        fmt.Println("stockings:", *s)
+
+        if *s {
+                strSantas = []string{"Anna", "Nina", "Laila", "John", "Nico", "Nicole", "Salim", "Susan"}               
+        } else {
+                strSantas = []string{"Anna", "John", "Nico", "Nicole", "Salim", "Susan"}
+        }           
         var k int
         fmt.Printf("Arranging %d Santas ...\n\n", len(strSantas))
         l := len(strSantas) - 1
@@ -16,14 +25,21 @@ func main() {
         rand.Seed(time.Now().UnixNano())
         rand.Shuffle(len(strSantas), func(i, j int) { strSantas[i], strSantas[j] = strSantas[j], strSantas[i] })
 
-        // fmt.Println("Dear grown-up gift-exchangers:\n")
+        if *s {
+                fmt.Println("Dear stocking stuffers:\n")
+        } else {    
+                fmt.Println("Dear grown-up gift-exchangers:\n")
+        }           
         for j := 0; j <= l; j++ {
                 if j == l {
-                        k = 0
-                } else {
+                        k = 0 
+                } else {    
                         k = j + 1
+                }           
+                if *s {
+                        fmt.Printf("Ho! Ho! Ho! %s, you will be making %s especially merry this year by putting a special gift in their stocking. \n", strSantas[j], strSantas[k])
+                } else {
+                        fmt.Printf("'Tis the season! %s, you will be making %s especially merry this year with a Christmas gift!\n", strSantas[j], strSantas[k])
                 }
-        fmt.Printf("Ho! Ho! Ho! %s, you will be making %s especially merry this year by putting a special gift in their stocking. \n", strSantas[j], strSantas[k])
-        //fmt.Printf("'Tis the season! %s, you will be making %s especially merry this year with a Christmas gift!\n", strSantas[j], strSantas[k])
-        }
-} //main
+        }           
+} //main 
